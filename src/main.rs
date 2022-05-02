b1f6f577f3dd2371d48a9424989329e704dee00b
r4v3n6101 <raven6107@gmail.com>
2022-05-02 15:15:14 +0300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{collections::HashMap, ffi::OsStr, sync::Arc};
 
 use clap::Parser;
-use indicatif::{HumanBytes, ProgressBar, ProgressStyle};
+use indicatif::{ProgressBar, ProgressStyle};
 use mcl_rs::{
     io::{file::Hierarchy, sync::Repository},
     process::GameCommand,
@@ -29,14 +29,13 @@ async fn main() -> anyhow::Result<()> {
     let file_hierarchy = Hierarchy::with_default_structure(&latest_release.id);
 
     println!("Fetching gamefiles index...");
-    let repository = Arc::new(Repository::fetch(client, file_hierarchy, latest_release).await?);
-    let pb = ProgressBar::new(repository.indices() as u64);
-    let ps = ProgressStyle::with_template(
-        "[{elapsed_precise}] {wide_bar:80.cyan/blue} {pos:>7}/{len:7} {msg}",
-    )
-    .unwrap()
-    .progress_chars("##-");
-    pb.set_style(ps);
+    let repository =
+        Arc::new(Repository::fetch(client, &file_hierarchy, latest_release.url.clone()).await?);
+    let pb = ProgressBar::new(repository.tracked_size());
+    pb.set_style(ProgressStyle::with_template("[{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes}")
+        .unwrap()
+        .with_key("eta", |state| format!("{:.1}s", state.eta().as_secs_f64()))
+        .progress_chars("#>-"));
 
     let pull_task = {
         let this = Arc::clone(&repository);
@@ -47,17 +46,8 @@ async fn main() -> anyhow::Result<()> {
         let this = Arc::clone(&repository);
         let pb = pb.clone();
         task::spawn_blocking(move || {
-            let mut prev = this.pulled_indices();
-            loop {
-                if this.pulled_indices() >= this.indices() {
-                    break;
-                }
-                if prev != this.pulled_indices() {
-                    prev = this.pulled_indices();
-
-                    pb.set_position(this.pulled_indices() as u64);
-                    pb.set_message(HumanBytes(this.downloader().downloaded_bytes()).to_string());
-                }
+            while this.downloader().downloaded_bytes() < this.tracked_size() {
+                pb.set_position(this.downloader().downloaded_bytes());
             }
         })
     };
@@ -65,17 +55,17 @@ async fn main() -> anyhow::Result<()> {
     pull_task.await??;
     update_pb_task.await?;
 
-    pb.finish();
+    pb.finish_and_clear();
 
     let features = HashMap::new();
     let command = GameCommand::new(
-        repository.hierarchy().gamedir.as_path(),
+        file_hierarchy.gamedir.as_path(),
         OsStr::new("java"),
         repository.version_info(),
         &features,
     );
     let command = command.build_with_default_params(
-        repository.hierarchy(),
+        &file_hierarchy,
         repository.version_info(),
         &args.username,
     );
