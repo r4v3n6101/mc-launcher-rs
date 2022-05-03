a2d708355b57f3701603e4d69e98b44300934e65
r4v3n6101 <raven6107@gmail.com>
2022-05-03 14:23:11 +0300

    Option to force download

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ struct Args {
     username: String,
     #[clap(long, default_value = "256")]
     concurrency: usize,
+    #[clap(long, short)]
+    force_download: bool,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -29,17 +31,29 @@ async fn main() -> anyhow::Result<()> {
     let file_hierarchy = Hierarchy::with_default_structure(&latest_release.id);
 
     println!("Fetching gamefiles index...");
-    let repository =
-        Arc::new(Repository::fetch(client, &file_hierarchy, latest_release.url.clone()).await?);
+    let mut repository =
+        Repository::fetch(client, &file_hierarchy, latest_release.url.clone()).await?;
+    println!("Tracking indices to download...");
+    if args.force_download {
+        repository.track_all();
+    } else {
+        repository.track_invalid().await?;
+    }
+    let repository = Arc::new(repository);
+
     let pb = ProgressBar::new(repository.tracked_size());
-    pb.set_style(ProgressStyle::with_template("[{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes}")
+    pb.set_style(
+        ProgressStyle::with_template(
+            "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes}",
+        )
         .unwrap()
         .with_key("eta", |state| format!("{:.1}s", state.eta().as_secs_f64()))
-        .progress_chars("#>-"));
+        .progress_chars("#>-"),
+    );
 
     let pull_task = {
         let this = Arc::clone(&repository);
-        task::spawn(async move { this.pull(args.concurrency).await })
+        task::spawn(async move { this.pull_tracked(args.concurrency).await })
     };
 
     let update_pb_task = {
