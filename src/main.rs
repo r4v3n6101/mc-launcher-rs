74f588ab631d240f1bf6cde54879a7efa2b73706
r4v3n6101 <raven6107@gmail.com>
2022-06-18 13:40:26 +0300

    Update to new api

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{collections::HashMap, ffi::OsStr, sync::Arc};
+use std::{collections::HashMap, ffi::OsStr};
 
 use clap::Parser;
 use indicatif::{ProgressBar, ProgressStyle};
 use mcl_rs::{
-    io::{file::Hierarchy, sync::Repository},
+    io::{download::Manager, file::Hierarchy, sync::RemoteRepository},
     process::GameCommand,
     resources::fetch_manifest,
 };
@@ -29,19 +29,22 @@ async fn main() -> anyhow::Result<()> {
     let manifest = fetch_manifest(&client).await?;
     let latest_release = manifest.latest_release().expect("latest not found");
     let file_hierarchy = Hierarchy::with_default_structure(&latest_release.id);
+    let downloader = Manager::new(client);
 
     println!("Fetching gamefiles index...");
-    let mut repository =
-        Repository::fetch(client, &file_hierarchy, latest_release.url.clone()).await?;
-    println!("Tracking indices to download...");
-    if args.force_download {
-        repository.track_all();
-    } else {
-        repository.track_invalid().await?;
-    }
-    let repository = Arc::new(repository);
+    let repository =
+        RemoteRepository::fetch(&downloader, &file_hierarchy, latest_release.url.clone()).await?;
+    downloader.reset();
 
-    let pb = ProgressBar::new(repository.tracked_size());
+    println!("Tracking indices to download...");
+    let tracked = if args.force_download {
+        repository.track_all()
+    } else {
+        repository.track_invalid().await?
+    };
+
+    let tracked_size = tracked.bytes_size();
+    let pb = ProgressBar::new(tracked_size);
     pb.set_style(
         ProgressStyle::with_template(
             "[{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes}",
@@ -51,23 +54,18 @@ async fn main() -> anyhow::Result<()> {
         .progress_chars("#>-"),
     );
 
-    let pull_task = {
-        let this = Arc::clone(&repository);
-        task::spawn(async move { this.pull_tracked(args.concurrency).await })
-    };
-
-    let update_pb_task = {
-        let this = Arc::clone(&repository);
+    let pb_update_task = {
         let pb = pb.clone();
+        let downloader = downloader.clone();
         task::spawn_blocking(move || {
-            while this.downloader().downloaded_bytes() < this.tracked_size() {
-                pb.set_position(this.downloader().downloaded_bytes());
+            while downloader.downloaded_bytes() < tracked_size {
+                pb.set_position(downloader.downloaded_bytes());
             }
         })
     };
 
-    pull_task.await??;
-    update_pb_task.await?;
+    tracked.pull(&downloader, args.concurrency).await?;
+    pb_update_task.await?;
 
     pb.finish_and_clear();
 
